b5c3d32ad6a358e27a25f1e01eece296dea3a88c
nourah <altamiminourahcs@gmail.com>
2022-08-01 23:05:07 +0300

    iv

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from './logo.png'
+export default function Navbar() {
+  return (
+    <nav class="navbar navbar-light ">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLYwuA81hg1IQGpBtZQr2NyHHMYYzAM2yFr4UaBMHsGbTF9Pl4T3yFCCnaKNTjc6uWLE0&usqp=CAU" alt="" width="80" height="80" class="d-inline-block align-text-top"/>
+    </a>
+  </div>
+</nav>
+  )
+}
---
+++ src/components/System.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function System() {
+  return (
+    <div>System</div>
+  )
+}
